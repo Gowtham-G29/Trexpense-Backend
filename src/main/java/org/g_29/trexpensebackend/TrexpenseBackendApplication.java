c74b71b3c593418405a3bad77661d3dec74afbdc
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2025-07-10 10:00:22 +0530

    initial commit

---
+++ src/main/java/org/g_29/trexpensebackend/TrexpenseBackendApplication.java
@@ -0,0 +1,13 @@
+package org.g_29.trexpensebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrexpenseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrexpenseBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/g_29/trexpensebackend/TrexpenseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.g_29.trexpensebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrexpenseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
